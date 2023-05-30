de91dea16dd46ac2686d44814e1b573dcaa01fb8
Enzo2023Dev <enzovelo7@gmail.com>
2023-05-30 03:49:07 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function compraverificada() {
+    alert("Compra Exitosa");
+    
+    window.location = "./index.html";
+}
+
+function cancelada() {
+    alert("Compra cancelada");
+    
+    window.location = "../products/stjw.html";
+}
